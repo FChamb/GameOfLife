c0bce59454c5e012764590f07c68a3cb511e4dd9
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-02-14 19:01:02 +0000

    Added a main method for testing and a toString method. Fixed from file constructor.

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,4 @@
 import java.io.File;
-
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -26,31 +25,60 @@ public class Board {
     }
 
     public Board(String file) {
-        ArrayList<String> boardTemp = new ArrayList<>();
+        ArrayList<String[]> boardTemp = new ArrayList<>();
         int rows = 0;
         int cols = 0;
         try {
             Scanner fileRead = new Scanner(new File(file));
             while (fileRead.hasNext()) {
                 String line = fileRead.nextLine();
+                String[] rowVals = new String[line.length()];
+                cols = 0;
                 for (int i = 0; i < line.length(); i++) {
-                    rows += line.length();
-                    boardTemp.add(String.valueOf(line.charAt(i)));
+                    rowVals[i] = String.valueOf(line.charAt(i));
+                    cols++;
                 }
-                cols++;
+                boardTemp.add(rowVals);
+                rows++;
             }
         } catch (FileNotFoundException e) {
             System.out.println("That game state does not exist: " + e.getMessage());
         }
         this.board = new Cell[rows][cols];
-        for (int i = rows; i < rows; i ++) {
+        for (int i = 0; i < rows; i ++) {
             for (int j = 0; j < cols; j++) {
-                if (boardTemp.get(i).equals(".")) {
+                if (boardTemp.get(i)[j].equals(".")) {
                     this.board[i][j] = new Cell();
                 } else {
                     this.board[i][j] = new Cell(true);
                 }
             }
         }
+    }
+
+    public Cell[][] getBoard() {
+        return this.board;
+    }
+
+    public String toString() {
+        String board = "";
+        for (int i = 0; i < this.board.length; i++) {
+            for (int j = 0; j < this.board[0].length; j++) {
+                board += this.board[i][j].toString();
+            }
+            if (i != this.board.length - 1) {
+                board += "\n";
+            }
+        }
+        return board;
+    }
+
+    /**
+     * A testing method to see how the board works in terminal view.
+     * @param args - the command line arguments, a game state
+     */
+    public static void main(String args[]) {
+        Board test = new Board(args[0]);
+        System.out.println(test);
     }
 }
