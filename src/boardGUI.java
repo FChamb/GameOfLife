013c85edaba7ec65a21821f012e16692e8a18db2
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-02-15 14:35:48 +0000

    Didn't really change much. Just adding a constructor.

--- src/boardGUI.java
+++ src/boardGUI.java
@@ -1,5 +1,11 @@
+import javax.swing.*;
+
 public class boardGUI {
+    private JFrame gameFrame;
+    private JMenu gameMenu;
     public boardGUI() {
+        gameFrame = new JFrame();
+        gameFrame.setTitle("Game of Life");
 
     }
 }
