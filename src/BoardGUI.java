ea464114ccc1196cdb5973a9c8d294f14a2bd8d4
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-02-16 17:09:13 +0000

    Worked on GUI. Implemented buttons which color match specification and print board state.

--- src/BoardGUI.java
+++ src/BoardGUI.java
@@ -4,10 +4,12 @@ import java.awt.*;
 public class BoardGUI {
     private JFrame gameFrame;
     private JMenu gameMenu;
+    private Board board;
+
     public BoardGUI(Board board) {
         gameFrame = new JFrame();
+        this.board = board;
         gameFrame.setTitle("Game of Life");
-        gameFrame.setSize(new Dimension(1000, 1000));
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameFrame.setLayout(new GridLayout(board.getBoard().length, board.getBoard()[0].length));
         addButtons(gameFrame, board.getBoard().length);
@@ -19,8 +21,17 @@ public class BoardGUI {
         JButton button;
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length; j++) {
-                button = new JButton(i + "," + j);
-                button.setPreferredSize(new Dimension(50, 50));
+                button = new JButton();
+                button.setPreferredSize(new Dimension(20, 20));
+                button.setOpaque(true);
+                button.setBorderPainted(false);
+                if (this.board.getBoard()[i][j].isAlive()) {
+                    button.setBackground(Color.WHITE);
+                    button.setForeground(Color.WHITE);
+                } else {
+                    button.setBackground(Color.BLACK);
+                    button.setForeground(Color.BLACK);
+                }
                 frame.add(button);
             }
         }
--- src/RunGame.java
+++ src/RunGame.java
@@ -1,6 +1,6 @@
 public class RunGame {
     public static void main(String[] args) {
-        Board gameBoard = new Board("test.gol");
+        Board gameBoard = new Board();
         BoardGUI gameVisual = new BoardGUI(gameBoard);
     }
 }
