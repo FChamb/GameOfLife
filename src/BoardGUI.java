91a6e413651842a4e8e76dbc6378590fceff1e72
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-02-17 17:32:24 +0000

    Messing with color swap and next generation

--- src/BoardGUI.java
+++ src/BoardGUI.java
@@ -7,29 +7,50 @@ import java.awt.event.MouseEvent;
 
 public class BoardGUI {
     private JFrame gameFrame;
-    private JMenu gameMenu;
+    private JPanel grid;
     private Board board;
+    private JButton[][] buttons;
 
     public BoardGUI(Board board) {
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         gameFrame = new JFrame();
         this.board = board;
         gameFrame.setTitle("Game of Life");
-        gameFrame.setSize(screen);
+        gameFrame.setSize(new Dimension(screen.width, screen.height - gameFrame.getInsets().top));
         gameFrame.setResizable(false);
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel frame = new JPanel();
-        frame.setLayout(new GridLayout(this.board.getWidth(), this.board.getHeight()));
-        JButton button;
+        grid = new JPanel();
+        grid.setLayout(new GridLayout(this.board.getWidth(), this.board.getHeight()));
+        this.buttons = new JButton[this.board.getWidth()][this.board.getHeight()];
         for (int i = 0; i < this.board.getWidth(); i++) {
             for (int j = 0; j < this.board.getHeight(); j++) {
-                button = new JButton();
-                button.setBackground(Color.BLACK);
-                frame.add(button);
+                final int row = i;
+                final int col = j;
+                buttons[i][j] = new JButton();
+                if (this.board.getBoard()[i][j].isAlive()) {
+                    buttons[i][j].setBackground(Color.WHITE);
+                } else {
+                    buttons[i][j].setBackground(Color.BLACK);
+                }
+                buttons[i][j].addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        changeCell(row, col);
+                        board.changeCell(row, col);
+                    }
+                });
+                grid.add(buttons[i][j]);
             }
         }
-        frame.setBorder(new EmptyBorder(100, 100, 100, 100));
-        gameFrame.add(frame);
+        grid.setBorder(new EmptyBorder(100, 100, 100, 100));
+        gameFrame.add(grid);
         gameFrame.setVisible(true);
+    }
+
+    public void changeCell(int row, int col) {
+        if (this.board.getBoard()[row][col].isAlive()) {
+            this.buttons[row][col].setBackground(Color.WHITE);
+        } else {
+            this.buttons[row][col].setBackground(Color.BLACK);
+        }
     }
 }
--- src/RunGame.java
+++ src/RunGame.java
@@ -1,6 +1,14 @@
 public class RunGame {
     public static void main(String[] args) {
         Board gameBoard = new Board("test.gol");
-        BoardGUI gameVisual = new BoardGUI(gameBoard);
+        gameBoard.nextGeneration();
+        //BoardGUI gameVisual = new BoardGUI(gameBoard);
+        /**
+        int i = 0;
+        while (i < 100) {
+            gameBoard.nextGeneration();
+            i++;
+        }
+         */
     }
 }
